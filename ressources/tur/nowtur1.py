9d9c6d9e232ae4cbece58d3e09dc244db9abbc98
KarabagIsAzerbaijan <vugarmelikov@gmail.com>
2025-11-23 14:23:18 +0400

    Update nowtur1.py

--- ressources/tur/nowtur1.py
+++ ressources/tur/nowtur1.py
@@ -1,40 +1,26 @@
-import os
 import requests
 import dropbox
-from urllib.parse import urlparse
 
-# Secrets vəya environment variables
-ERSTRM_URL = os.environ.get("ERSTRM_URL")
-DASTRM_URL = os.environ.get("DASTRM_URL")
-DROPBOX_TOKEN = os.environ.get("DROPBOX_TOKEN")
+# Dropbox token və yollar
+DROPBOX_TOKEN = "sənin_dropbox_token"
+DROPBOX_ERSTRM_PATH = "/nowtur1/ERSTRM/ERSTRM.m3u8"
+DROPBOX_DASTRM_PATH = "/nowtur1/DASTRM/DASTRM.m3u8"
 
-# Dropbox qovluqları
-DROPBOX_ERSTRM_PATH = "/nowtur1/erstrm/"
-DROPBOX_DASTRM_PATH = "/nowtur1/dastrm/"
+# Tokenləri çək
+def get_tokened_link(url):
+    resp = requests.get(url, verify=False)
+    # regex ilə tokenli linki tap
+    return tokened_link
+
+# Faylı Dropbox-a yaz
+def upload_to_dropbox(dbx, path, content):
+    dbx.files_upload(content.encode(), path, mode=dropbox.files.WriteMode.overwrite)
 
 dbx = dropbox.Dropbox(DROPBOX_TOKEN)
 
-def download_segments(m3u8_url):
-    resp = requests.get(m3u8_url)
-    resp.raise_for_status()
-    lines = resp.text.splitlines()
-    ts_links = [line for line in lines if line and not line.startswith("#")]
-    return ts_links
+erstrm_link = get_tokened_link("https://www.nowtv.com.tr/canli-yayin")
+dastrm_link = get_tokened_link("...")
 
-def upload_to_dropbox(ts_url, dropbox_path):
-    filename = os.path.basename(urlparse(ts_url).path)
-    resp = requests.get(ts_url)
-    resp.raise_for_status()
-    dbx.files_upload(resp.content, dropbox_path + filename, mode=dropbox.files.WriteMode.overwrite)
-    print(f"Uploaded: {dropbox_path}{filename}")
-
-def process_stream(m3u8_url, dropbox_folder):
-    ts_links = download_segments(m3u8_url)
-    for ts in ts_links:
-        upload_to_dropbox(ts, dropbox_folder)
-
-if __name__ == "__main__":
-    if ERSTRM_URL:
-        process_stream(ERSTRM_URL, DROPBOX_ERSTRM_PATH)
-    if DASTRM_URL:
-        process_stream(DASTRM_URL, DROPBOX_DASTRM_PATH)
+# Faylları Dropbox-a yaz
+upload_to_dropbox(dbx, DROPBOX_ERSTRM_PATH, erstrm_link)
+upload_to_dropbox(dbx, DROPBOX_DASTRM_PATH, dastrm_link)
